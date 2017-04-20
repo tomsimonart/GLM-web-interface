7ec810838a683fe32331fc4f0c1e06ae7462e5fd
Infected <tom.simonart@gmail.com>
2017-04-20 02:50:06 +0200

    [pep8]

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from flask import Flask, url_for, request, render_template
+from flask import Flask, render_template
 from GLM import glm
 app = Flask(__name__)
 
 glm.PLUGIN_PACKAGE = "GLM.source.plugins"
 PLUGIN_DIRECTORY = "./GLM/source/" + glm.PLUGIN_PREFIX + "/"
 
+
 @app.route('/')
 def index():
-    plugins = list(map(lambda x: x.replace('_', ' ').replace('.py', ''), glm.plugin_scan(PLUGIN_DIRECTORY)))
+    plugins = list(map(
+        lambda x: x.replace('_', ' ').replace('.py', ''),
+        glm.plugin_scan(PLUGIN_DIRECTORY)
+    ))
     return render_template('main.html', plugins=enumerate(plugins))
+
 
 @app.route('/plugin/<id>')
 def select_plugin(id):
