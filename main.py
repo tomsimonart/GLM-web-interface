9249fbf88395f78ddb7331f2baed67a855539ccb
urlab <tom.simonart@gmail.com>
2017-10-11 14:14:14 +0200

    Threading but it's not working

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from flask import Flask, render_template
 from GLM import glm
+import threading
+
+
 app = Flask(__name__)
 
 glm.PLUGIN_PACKAGE = "GLM.source.plugins"
 PLUGIN_DIRECTORY = "./GLM/source/" + glm.PLUGIN_PREFIX + "/"
+
+class Loader(threading.Thread):
+    def __init__(self, threadID, name, counter, id):
+        super(Loader, self).__init__()
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+        self.id = id
+
+    def run(self):
+        print("Starting thread.")
+        glm.plugin_loader(glm.plugin_scan(PLUGIN_DIRECTORY)[int(self.id)])
+        print('Done.')
 
 
 @app.route('/')
@@ -18,5 +34,7 @@ def index():
 @app.route('/plugin/<id>')
 def select_plugin(id):
     print(id)
-    glm.plugin_loader(glm.plugin_scan(PLUGIN_DIRECTORY)[int(id)])
+    plugin_loader = Loader(1, "loader_1", 1, id)
+    plugin_loader.
+    plugin_loader.start()
     return ''
