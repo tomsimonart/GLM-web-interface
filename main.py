a349c108977c6fabe62d42d0911b8ad77420e6e3
Infected <tom.simonart@gmail.com>
2018-02-19 09:36:26 +0100

    [dev] server.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+import socket
 from GLM import glm
+from random import randint
 from multiprocessing import Process
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 glm.PLUGIN_PACKAGE = "GLM.source.plugins"
 PLUGIN_DIRECTORY = "./GLM/source/" + glm.PLUGIN_PREFIX + "/"
 plugin_loader = None
+
+server_addr = 'localhost'
+server_port = 9998
 
 @app.route('/')
 def index():
@@ -28,9 +34,27 @@ def select_plugin(id):
 
 @app.route('/plugin/<int:id>/webview')
 def webview(id):
-    return render_template('webview.html', id=id)
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((server_addr, server_port))
 
-@app.route('/plugin/<int:id>/<event>')
+    # Web client response
+    response = "web_client;" + "req_view"
+    client.send(response.encode())
+
+    response = client.recv(512)
+    client.close()
+    return render_template('webview.html', data=response.decode())
+
+@app.route('/plugin/<int:id>/<str:event>')
 def event(id, event):
-    # Need to open a event server on each plugins
-    pass
+    # Need to open a event client on each plugins
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((server_addr, server_port))
+
+    response = "web_client" + event
+    client.send(response.encode())
+
+    response = client.recv(512) # Drop response
+    client.close()
+
+    return None
--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from sys import argv
 from time import sleep
 from GLM.source.libs.rainbow import msg
 
+SEP_CHAR = ';'
 end = False
 
 addr = "localhost"
@@ -17,30 +18,79 @@ if len(argv) >= 2 and argv[1].isdecimal():
 server_addr = (addr, port)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server.setblocking(0)
 server.bind(server_addr)
 server.listen(5)
+
+def handle(client, client_message=None, transmit=True):
+    response = ""
+
+    # Message is empty
+    if client_message == None or client_message == ' ':
+        response = "Empty message"
+
+    # Message has the wrong format
+    elif client_message.rfind(SEP_CHAR) < 0:
+        response = "Bad message"
+
+    else: # Decompose message
+        msg(client_message)
+        user = ""
+        message = SEP_CHAR
+        try:
+            user, message = client_message.split(SEP_CHAR)
+            if message == "close":
+                transmit = False
+
+            elif user == "web_client":
+                msg("web client", 1)
+                # client.send("hello web client".encode())
+                response = "hello web client"
+                # Processing web client requests
+            elif user == "plugin":
+                msg("plugin", 1)
+                # client.send("hello plugin".encode())
+                response = "hello plugin"
+                # Processing plugin requests
+            else:
+                msg("bad user", 2)
+                # client.send("bad user".encode())
+                response = "bad user"
+        except IndexError:
+            # client.send("wrong format".encode())
+            response = "bad user"
+
+    return response, transmit
 
 def handle_client():
     global end
 
     # Accepting client connection
     client, addr = server.accept()
-    msg("New client", 0, "Server", addr)
+    msg("New client", 0, "Server", addr, client)
 
-    # Getting client packet
-    client_message = client.recv(512).decode()
-    msg("Message", 0, "Client", client_message)
+    transmit = True
+    while transmit:
+        # Getting client packet
+        received = False
+        while not received:
+            client_message = client.recv(512).decode()
+            if len(client_message) > 0:
+                received = True
+        msg("Message", 0, "Client", client_message)
 
-    if client_message == "exit":
-        msg("Terminating",  2, "Server")
-        client.send("[*] Terminating".encode()) # Last message
-        server.close()
-        end = True
-        return None
+        if client_message == "exit": # Close server
+            client.send("[*] Terminating".encode()) # Last message
+            client.close()
+            sleep(1)
+            transmit = False
+            end = True
 
-    else:
-        client.send("[*] Received: {}".format(client_message).encode())
-
+        else:
+            # Answering to client after handle
+            response, transmit = handle(client, client_message, transmit)
+            client.send(response.encode())
+            msg("Response", 0, "Server", response)
 
     return None
 
@@ -53,6 +103,8 @@ if __name__ == "__main__":
                 client_handler.start()
                 client_handler.join() # Waiting for thread to end
             else:
+                server.close()
+                msg("Terminating",  2, "Server")
                 break
     except KeyboardInterrupt:
         print()
