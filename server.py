42f21b3def0612f0d0376f2920cc6bc8ab18d945
owner <tom.simonart@gmail.com>
2018-02-20 11:36:40 +0100

    [add] event_queue & data_list

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import socket
 import threading
 from sys import argv
 from time import sleep
+from queue import Queue
 from GLM.source.libs.rainbow import msg
 
 SEP_CHAR = ';'
@@ -23,76 +24,73 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Re-use port
 server.bind(server_addr)
 server.listen(5)
 
-# def handle(client, client_message, user, transmit):
-#     msg("handle", 3)
-#     response = "" # Will be sent back to client
+# def handle(client, user, client_id, transmit):
+#     response = " " # Will be sent to client
+#     client_request = client.recv(BUFFSIZE).decode() # 2 recv
+#     msg(client_id, 0, user, client_request)
 #
-#     # Message is empty
-#     if client_message == None or client_message == ' ':
-#         response = "Empty message"
+#     # Plugin
+#     if user == "plugin":
+#         response = "plugin handle"
 #
-#     # Message has the wrong format
-#     elif client_message.rfind(SEP_CHAR) < 0:
-#         response = "Bad message"
+#     # Web client
+#     elif user == "web_client":
+#         response = "web_client handle"
 #
-#     else: # Decompose message
-#         msg(client_message)
-#         user = ""
-#         message = SEP_CHAR
-#         try:
-#             user, message = client_message.split(SEP_CHAR)
-#             if message == "close":
-#                 transmit = False
+#     else:
+#         transmit = False
 #
-#             elif user == "web_client":
-#                 msg("web client", 1)
-#                 response = "<a>tperale</a>"
-#                 # Processing web client requests
-#             elif user == "plugin":
-#                 msg("plugin", 1)
-#                 response = "hello plugin"
-#                 # Processing plugin requests
-#             else:
-#                 msg("bad user", 2)
-#                 response = "bad user"
-#         except IndexError:
-#             response = "bad user"
+#     transmit = False
 #
 #     return response, transmit
 
-def handle(client, user, transmit):
-    response = " " # Will be sent to client
-    client_request = client.recv(BUFFSIZE).decode() # 2 recv
+def handle_plugin(plugin, plugin_id, transmit):
+    # Check if plugin is outdated
+    if plugin_id < data_list[0]:
+        transmit = False
 
-    # Plugin
-    if user == "plugin":
-        response = "plugin handle"
-
-    # Web client
-    elif user == "web_client":
-        response = "web_client handle"
-
+    # Update web data and read queue (async ?)
     else:
         transmit = False
 
+    return transmit
+
+def handle_web_client(web_client, web_client_id, transmit):
+    # Detect if client is still connected
+    response = web_client.recv(BUFFSIZE).decode()
+    msg(response, 0, "web_client", web_client_id)
     transmit = False
+    # Put data in queue if there is an event
+    # Send back web data to web client if web_client requests it
+    return transmit
 
-    return response, transmit
+def handle_client(client, addr, user, client_id):
+    global end # Close server
+    transmit = True # Close transmission with client
 
-def handle_client(client, addr, user):
-    global end
-
-    transmit = True
     while transmit:
-        response, transmit = handle(client, user, transmit)
-        msg("Responding", 3, response, addr)
-        client.send(response.encode())
+        sleep(0.5)
+        if user == "plugin":
+            transmit = handle_plugin(client, client_id, transmit)
 
+        elif user == "web_client":
+            transmit = handle_web_client(client, client_id, transmit)
+
+        # response, transmit = handle(client, user, client_id, transmit)
+        # msg("Responding", 3, response, addr)
+        # client.send(response.encode())
+
+    # Message for thread ending
+    msg(user + "_" + str(client_id), 2, "Thread", transmit)
     return None
 
 if __name__ == "__main__":
+    event_queue = Queue() # All events coming from web clients
+    data_list = [0, "", 0] # Html data or forms container [id, data, flag]
+
     try:
         msg("Starting", 1, "Server", server_addr)
+        client_id = 0
         while True:
             if not end:
                 # Accepting client connection
@@ -104,19 +102,26 @@ if __name__ == "__main__":
                 if user == "plugin" or user == "web_client":
                     # User accepted
                     client.send(b"a:client_connected")
-                    msg("Accepted", 1, "Server", user)
+                    msg("Accepted", 1, "Server", user, client_id)
+
+                    # If user is plugin then change id in data_list[0]
+                    # This is used to verify plugin
+                    if user == "plugin":
+                        data_list[0] = client_id
                 else:
                     # Bad user
                     client.send(b"e:bad_user")
-                    msg("Refused", 2, "Server", user)
+                    msg("Refused", 2, "Server", user, client_id)
 
                 client_handler = threading.Thread(
-                    name=user,
+                    name=user + "_" + str(client_id),
                     target=handle_client,
-                    args=(client, addr, user),
+                    args=(client, addr, user, client_id),
                     daemon=True
                     )
                 client_handler.start()
+                msg(client_handler.getName(), 0, "Thread", user, client_id)
+                client_id += 1
             else:
                 server.close()
                 msg("Terminating",  2, "Server")
