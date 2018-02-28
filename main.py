28f91d80a351a8ecb3b90df08896df7c32ffceb4
tomsimonart <tom.simonart@gmail.com>
2018-02-28 11:04:10 +0100

    [fix] rewrite

--- main.py
+++ main.py
@@ -3,21 +3,18 @@ import socket
 import traceback
 from GLM import glm
 from random import randint
-from multiprocessing import Process, Queue
 from GLM.source.libs.rainbow import msg
+from multiprocessing import Process, Queue
 from flask import Flask, render_template
 
 app = Flask(__name__)
 app.debug = True
 
-glm.PLUGIN_PACKAGE = "GLM.source.plugins"
-PLUGIN_DIRECTORY = "./GLM/source/" + glm.PLUGIN_PREFIX + "/"
-plugin_loader = None
-
 server_addr = 'localhost'
 server_port = 9999
 
 BUFFSIZE = 512
+
 
 @app.route('/')
 def index():
@@ -31,43 +28,33 @@ def index():
 @app.route('/plugin/<int:id>')
 def select_plugin(id):
     msg("select_plugin")
-    global plugin_loader
-    print(id)
-    if plugin_loader is not None:
-        plugin_loader.terminate()
-    plugin_loader = Process(target=glm.plugin_loader, args=(glm.plugin_scan(PLUGIN_DIRECTORY)[int(id)],))
-    plugin_loader.start()
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((server_addr, server_port))
+    client.send(b"web_client")
+    status = client.recv(BUFFSIZE).decode()
+    if status == "a:client_connected":
+        event = json.dumps({"LOADPLUGIN": id}).encode()
+        client.send(event)
+        status = client.recv(BUFFSIZE).decode()
+
+    client.send(b"EOT")
+    client.close()
     return ''
 
 @app.route('/plugin/<int:id>/webview')
 def webview(id):
     """ Request data from server then render it's template
     """
-
     data = '' # No data
-
     # Connection to server
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((server_addr, server_port))
-
-    # Send user name
-    client.send("web_client".encode())
-
-    # Can connect ?
+    client.send(b"web_client")
     status = client.recv(BUFFSIZE).decode()
     if status == "a:client_connected":
-        request = json.dumps({"method": "GET", "data": "refresh"}).encode()
-        client.send(request)
-        print("blocked")
-        # Getting data
-        response = client.recv(BUFFSIZE).decode()
-        msg(response)
-        if response:
-            data = json.loads(response)
-        print(data)
-
-    else:
-        msg("Can't send events", 3)
+        event = json.dumps({"READ": "REFRESH"}).encode()
+        client.send(event)
+        data = client.recv(BUFFSIZE).decode()
 
     client.send(b"EOT")
     client.close()
@@ -79,11 +66,15 @@ def event(id, event):
     # Need to open a event client on each plugins
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((server_addr, server_port))
+    client.send("web_client")
+    status = client.recv(BUFFSIZE).decode()
+    if status == "a:client_connected":
+        event = json.dumps(
+            {'WRITE': {'0_input': 'sample input', '1_button': 'pressed'}}
+            ).encode()
+        client.send(event)
+        status = client.recv(BUFFSIZE).decode()
 
-    response = "web_client" + event
-    client.send(response.encode())
-
-    response = client.recv(BUFFSIZE) # Drop response
+    client.send(b"EOT")
     client.close()
-
     return ''
--- server.py
+++ server.py
@@ -3,9 +3,11 @@
 import json
 import socket
 import threading
+from GLM import glm
 from sys import argv
 from time import sleep
 from queue import Queue
+import multiprocessing
 from GLM.source.libs.rainbow import msg
 import traceback
 
@@ -14,6 +16,10 @@ end = False
 
 addr = "localhost"
 port = 9999
+
+glm.PLUGIN_PACKAGE = "GLM.source.plugins"
+PLUGIN_DIRECTORY = "./GLM/source/" + glm.PLUGIN_PREFIX + "/"
+plugin_loader = None
 
 if len(argv) >= 2 and argv[1].isdecimal():
     port = int(argv[1])
@@ -25,55 +31,67 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Re-use port
 server.bind(server_addr)
 server.listen(5)
 
+# All events coming from web clients
+event_queue = Queue()
+# Html data or forms container [id, data, refresh_flag_int]
+data_list = [0, "", 0]
+
+plugin_loader_queue = multiprocessing.Queue()
+
 
 def handle_plugin(plugin, plugin_id, transmit):
     """ Reads events from the queue then request data to the plugin and
     stores it back to the data list
     """
     client.send(b"a:client_connected")
-
     while transmit:
-        # Check if plugin is outdated
-        if plugin_id < data_list[0]:
-            transmit = False
-
         status = plugin.recv(BUFFSIZE).decode()
 
-        if status == "READY":
-            # Gets the events from the queue
-            event = event_queue.get() # Blocking
-            plugin.send(json.dumps(event).encode())
-            msg("sent data", 0, "Plugin", event)
+        # event_queue get REFRESH
 
-            # receive data from plugin
-            response = plugin.recv(BUFFSIZE).decode()
-            msg("got data", 0, "Plugin", response)
+        # event_queue get DICT type
 
-            # End of transmission with the plugin
-            if response == "EOT" or response == "":
-                transmit = False
+    # while transmit:
+    #     # Check if plugin is outdated
+    #     if plugin_id < data_list[0]:
+    #         transmit = False
+    #
+    #     status = plugin.recv(BUFFSIZE).decode()
+    #
+    #     if status == "READY":
+    #         # Gets the events from the queue
+    #         event = event_queue.get() # Blocking
+    #         plugin.send(json.dumps(event).encode())
+    #         msg("sent data", 0, "Plugin", event)
+    #
+    #         # receive data from plugin
+    #         response = plugin.recv(BUFFSIZE).decode()
+    #         msg("got data", 0, "Plugin", response)
+    #
+    #         # End of transmission with the plugin
+    #         if response == "EOT" or response == "":
+    #             transmit = False
+    #
+    #         else:
+    #             # Update data in data_list and set refresh_flag to True
+    #             msg(response)
+    #             print(plugin)
+    #             msg(json.loads(response), 3)
+    #             data_list[1] = json.loads(response)
+    #             data_list[2] += 1
+    #
+    #     elif status == "EOT" or status == "":
+    #         transmit = False
+    #
+    #     else:
+    #         transmit = False
+    #
+    # return transmit
 
-            else:
-                # Update data in data_list and set refresh_flag to True
-                msg(response)
-                print(plugin)
-                msg(json.loads(response), 3)
-                data_list[1] = json.loads(response)
-                data_list[2] += 1
 
-        elif status == "EOT" or status == "":
-            transmit = False
-
-        else:
-            transmit = False
-
-    return transmit
-
-
-def handle_web_client(web_client, web_client_id, transmit):
-    """ This function gets events from the web client and adds them to
-    the queue, plugin should handle the events and send back adequate data
-    that will then be sent back from here to the client
+def handle_web_client(web_client, web_client_id, transmit, plugin_loader):
+    """ This function should be able to receive events from clients and
+    execute actions like (spawning a plugin process, sending web data back...)
     """
     client.send(b"a:client_connected")
 
@@ -81,23 +99,54 @@ def handle_web_client(web_client, web_client_id, transmit):
 
         # Add event to queue
         event = web_client.recv(BUFFSIZE).decode()
-        if event == "EOT" or event == "":
+        if not event or event == "EOT":
             transmit = False
 
         else:
-            event_queue.put(json.loads(event))
+            event_read = json.loads(event)
 
-            # Wait for data_list to get an update then send it back to web_cli
-            refresh_flag = data_list[2]
-            refreshed = False
-            while not refreshed:
-                if refresh_flag < data_list[2]:
-                    data_list[2] += 1
-                    web_client.send(json.dumps(data_list[1]).encode())
-                    refreshed = True
-                    break
+            # Plugin loading phase
+            event_test = event_read.pop("LOADPLUGIN", None)
+            if event_test is not None:
+                msg("Plugin phase", 1, "web_client_handler", web_client_id)
+                if plugin_loader is not None:
+                    plugin_loader_queue.put("END")
+                    plugin_loader_queue.join() # Wait for process to end
+                plugin_loader = multiprocessing.Process(
+                    target=glm.plugin_loader,
+                    daemon=False,
+                    args=(
+                        glm.plugin_scan(PLUGIN_DIRECTORY)[event_test], # id
+                        plugin_loader_queue,
+                        True, # start
+                        False, # matrix
+                        True, # show
+                        False # guishow
+                        )
+                    )
+                plugin_loader.start()
+                msg("plugin loaded", 0, "Status")
+                web_client.send(b"status:plugin_loaded")
 
-            transmit = False
+            # Getting data phase
+            event_test = event_read.pop("READ", None)
+            if event_test is not None:
+                msg("Getting data phase", 1, "web_client_handler", web_client_id)
+                if event_test == "REFRESH":
+                    # TODO web_client.send(data_list[1].encode())
+                    data_state = data_list[2]
+                    event_queue.put(event_test)
+                    while data_state >= data_list[2]:
+                        # Event is waiting for refresh
+                        sleep(0.2)
+                    web_client.send(data_list[1].encode())
+
+            # Sending events phase
+            event_test = event_read.pop("WRITE", None)
+            if event_test is not None:
+                msg("Sending events phase", 1, "web_client_handler", web_client_id)
+                event_queue.put(event_test)
+                web_client.send("status:got_event")
 
     return transmit
 
@@ -111,7 +160,12 @@ def handle_client(client, addr, user, client_id):
             transmit = handle_plugin(client, client_id, transmit)
 
         elif user == "web_client":
-            transmit = handle_web_client(client, client_id, transmit)
+            transmit = handle_web_client(
+                client,
+                client_id,
+                transmit,
+                plugin_loader
+                )
 
     # Message for thread ending
     msg(user + "_" + str(client_id), 2, "Thread", transmit)
@@ -119,18 +173,12 @@ def handle_client(client, addr, user, client_id):
 
 
 if __name__ == "__main__":
-    event_queue = Queue() # All events coming from web clients
-
-    # Html data or forms container [id, data, refresh_flag_int]
-    data_list = [0, "", 0]
-
     try:
         msg("Starting", 1, "Server", server_addr)
         client_id = 0
         while True:
             if not end:
                 # Accepting client connection
-                msg("Waiting for client")
                 client, addr = server.accept()
                 user = client.recv(BUFFSIZE).decode() # 1 recv
 
