6422500ad325569efe0a5c3e45566f2ead662435
tomsimonart <tom.simonart@gmail.com>
2018-05-21 23:41:52 +0200

    [dev] snake

--- GLM/source/plugins/snake.py
+++ GLM/source/plugins/snake.py
@@ -1,7 +1,6 @@
-from queue import Queue
+from random import randint
 from time import process_time
 from ..libs.text import Text
-from ..libs.rainbow import msg
 from ..libs.image import Image
 from ..libs.drawer import Drawer
 from ..libs.pluginbase import PluginBase
@@ -94,9 +93,7 @@ class Plugin(PluginBase):
     def difficulty_select(self):
         self.choose_difficulty = False
         self.screen.remove_all()
-        self.unregister('up')
-        self.unregister('down')
-        self.unregister('enter')
+        self.unregister_all()
         self.start_game()
 
     def start_game(self):
@@ -104,7 +101,7 @@ class Plugin(PluginBase):
         self.register('down', self.go_down)
         self.register('left', self.go_left)
         self.register('right', self.go_right)
-        self.snake = Snake()
+        self.snake = Snake(default_size=3, difficulty=self.difficulty)
 
     def go_right(self):
         self.snake.set_direction(0)
@@ -122,24 +119,67 @@ class Snake:
     def __init__(self, default_size=3, difficulty=0):
         """Snake game by Infected
         """
-        self.__game_canvas = Image()
+        self.__game_canvas = Image(64, 16)
         self.__drawer = Drawer(self.__game_canvas)
         self.__difficulty = 0
         self.__direction = 0
         self.__size = 3
+        self.__foods = []
+        self.__food_blink = False
         self.__body = []
-        self.__move_queue = Queue()
+        self.__position = ()
         self.__score = 0
+        self.__lost = False
+        if difficulty > 1:
+            self.__borders = ((1, 1), (14, 62))
+            self.__wall_collision = True
+        else:
+            self.__borders = ((0, 0), (15, 63))
+            self.__wall_collision = False
 
     def get_difficulty(self):
         return self.__difficulty
 
     def refresh(self):
         self.__game_canvas.blank()
-        if self.__difficulty > 1:
-            pass
+
         self.move()
-        return self.__game_canvas
+
+    def __check_collision(self):
+        # Check wall collisions
+        if self.__wall_collision:
+            if self.__position[0] < self.__borders[0][0]:
+                self.__lost = True
+            elif self.__position[0] > self.__borders[1][0]:
+                self.__lost = True
+            elif self.__position[1] < self.__borders[1][0]:
+                self.__lost = True
+            elif self.__position[1] > self.__borders[1][1]:
+                self.__lost = True
+
+        # Check tail collisions
+        if self.position in self.__body:
+            self.__lost = True
+
+    def __blink_food(self):
+        self.__food_blink = not self.__food_blink
+        return self.__food_blink
+
+    def __put_food(self):
+        border_0 = randint(*self.__borders[0])
+        border_1 = randint(*self.__borders[1])
+        if self.__foods.count((border_0, border_1)) == 0:
+            self.__foods.append((border_0, border_1))
+
+    def __eat_food(self, position):
+        if self.__foods.count(position) >= 1:
+            self.__foods.remove(position)
+            self.__score += 100
+            self.__size += 1
+            self.__put_food()
+
+    def check_lost(self):
+        return self.__lost
 
     def set_direction(self, direction):
         if type(direction) == int and direction in range(4):
@@ -148,4 +188,19 @@ class Snake:
         return False
 
     def move(self):
-        pass
+        op = self.__position # Old position
+        # Right
+        if self.__direction == 0:
+            self.__position = (op[0] + 1, op[1])
+        # Down
+        if self.__direction == 1:
+            self.__position = (op[0], op[1] + 1)
+        # Left
+        if self.__direction == 2:
+            self.__position = (op[0] - 1, op[1])
+        # Up
+        if self.__direction == 3:
+            self.__position = (op[0], op[1] - 1)
+
+    def get_game_canvas(self):
+        return self.__game_canvas
